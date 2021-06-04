f2ce8c00fac071a01b81d06547d57f9313b0b4af
Qi Xu <qxu6@circinus-39.ics.uci.edu>
2021-06-03 18:57:51 -0700

    update

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ import numpy as np
 
 def demo():
 
-    Y, X, A, optA = getdata(200, case=2, seed=2)
+    Y, X, A, optA = getdata(200, case=2, seed=1)
 
     mcitr = MCITR(depth_trt=3, depth_cov=3, width_trt=50, width_cov=50, width_embed=3)
-    history = mcitr.fit(Y, X, A, device="cpu", verbose=1, epochs=200, learning_rate=5e-2)
+    history = mcitr.fit(Y, X, A, device="cpu", verbose=1, epochs=50, learning_rate=5e-2)
 
     D = mcitr.predict(X, A)
     accuracy, value = mcitr.evaluate(Y, A, D, X, optA)
@@ -31,10 +31,6 @@ def demo():
     print("----accuracy: {0}----".format(accuracy))
     print("----value: {0}----".format(value))
 
-    # Y, X, A, optA = getdata(200, case=1)
-
-    # plt.hist(Y)
-    # plt.savefig("python_outcome_hist.jpg", dpi=300)
 
 if __name__ == "__main__":
 
